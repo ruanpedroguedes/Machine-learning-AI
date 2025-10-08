7faedb81767ebf33dcdcd763b93980e570c48b8e
João Lucas <lgbr212@gmail.com>
2025-10-08 15:05:26 -0300

    fix: ajustando imports e diretorios

--- pages/supervisionado.py
+++ pages/supervisionado.py
@@ -1,8 +1,9 @@
+import streamlit as st
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from supervisionado.app import main
+from Supervisionado.app import main
 
 st.set_page_config(page_title="Análise Supervisionada", layout="wide")
 main()
