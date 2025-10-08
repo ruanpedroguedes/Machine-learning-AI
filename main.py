4338da61b0eab54d1cab7b68e3f4d32c40cc46a0
João Lucas <lgbr212@gmail.com>
2025-10-08 14:39:41 -0300

    feat(main): criacao de arquivo principal para streamlit

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Projeto - Machine Learning", layout="wide")
+st.title("Machine Learning - Projeto de Otimização de Delegacias")
+st.write("Navegue para as diferentes análises:")
